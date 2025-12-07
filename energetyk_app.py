43e771bf8c9bd3a578cbc9977009d053939cc550
Tomasz Krauze <tomkrauze@gmail.com>
2025-12-07 13:17:58 +0100

    Added different age variants

--- energetyk_app.py
+++ energetyk_app.py
@@ -3,5 +3,13 @@ wiek = input("Podaj wiek uzytkownika: ")
 if wiek.isdigit() == False:
     exit("Wiek musi byc liczba calkowita. Zamykam aplikację")
 wiek=int(wiek)
-if wiek>=18:
+if wiek<0:
+    exit("Wiek nie może być ujemny. Zamykam aplikację")
+elif wiek>=0 and wiek<18:
+    exit("Jesteś niepełnoletni. Zamykam aplikację")
+elif wiek>=18 and wiek<=100:
     print("Witamy w apce. Mozesz kupować u nas energetyki")
+elif wiek>=100:
+    exit("Jesteś za stary. Zamykam aplikację")
+else:
+    print("EXCEPTION NOT HANDLED")
