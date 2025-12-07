17773fee1ef5e0ed03f98deb923ba2b6673e0507
Tomasz Krauze <tomkrauze@gmail.com>
2025-12-07 13:39:38 +0100

    Float validation

--- energetyk_app.py
+++ energetyk_app.py
@@ -1,8 +1,11 @@
 wiek = input("Podaj wiek uzytkownika: ")
+
 #sprawdzenie czy wiek jest liczba calkowitą
-if wiek.isdigit() == False:
-    exit("Wiek musi byc liczba calkowita. Zamykam aplikację")
-wiek=int(wiek)
+try:
+    wiek = float(wiek)
+except:
+    exit("Wiek nie jest liczba. Zamykam aplikację")
+
 if wiek<0:
     exit("Wiek nie może być ujemny. Zamykam aplikację")
 elif wiek>=0 and wiek<18:
@@ -12,4 +15,4 @@ elif wiek>=18 and wiek<=100:
 elif wiek>100:
     exit("Jesteś za stary. Zamykam aplikację")
 else:
-    print("EXCEPTION NOT HANDLED")
+    print("UNEXPECTED ERROR")
