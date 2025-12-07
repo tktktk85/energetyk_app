4e17dcc9dc1260260284f559020205d6a45eab1c
Tomasz Krauze <tomkrauze@gmail.com>
2025-12-07 11:19:39 +0100

    initial commit app v 1.0

---
+++ energetyk_app.py
@@ -0,0 +1,7 @@
+wiek = input("Podaj wiek uzytkownika: ")
+#sprawdzenie czy wiek jest liczba calkowitą
+if wiek.isdigit() == False:
+    exit("Wiek musi byc liczba calkowita. Zamykam aplikację")
+wiek=int(wiek)
+if wiek>=18:
+    print("Witamy w apce. Mozesz kupować u nas energetyki")
